9298dade8e79e25062df3805cd43d644cb437458
Oscar Poudel <opoudel27@gmail.com>
2026-02-08 20:50:35 -0500

    Images fixed plus non required part removed

--- src/app/components/layout/logo/index.tsx
+++ src/app/components/layout/logo/index.tsx
@@ -15,7 +15,7 @@ const Logo = (props: { sticky: boolean }) => {
                 priority={true}
                 className='hidden xsm:block'
             />
-            <Image src={sticky ? "/images/logo/favicondark.svg" : "/images/logo/favicon.svg"} alt='logo' width={120} height={60} className='block xsm:hidden' />
+            <Image src={sticky ? "/images/logo/favicondark.svg" : "/images/logo/WhiteLogo.svg"} alt='logo' width={100} height={60} className='block xsm:hidden' />
         </Link>
     );
 };
